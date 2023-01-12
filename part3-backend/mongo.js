70d1b6d70edaeeb8ff1b466fb79a491318b53c43
shmaiii <luongxuanmai2109@gmail.com>
2023-01-12 15:09:53 -0800

    mongo.js 3.12 command-line database

--- part3-backend/mongo.js
+++ part3-backend/mongo.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
-if (process.argv.length < 5) {
-    console.log('params missing');
+if (process.argv.length < 3) {
+    console.log('password missing');
     process.exit(1);
 }
 
@@ -17,14 +17,28 @@ const personSchema = new mongoose.Schema({
 });
 const Person = mongoose.model('Person', personSchema);
 
-const name = process.argv[3];
-const number = process.argv[4];
+// if password is the only parameter => display all entries
+if (process.argv.length === 3) {
+    Person.find({})
+    .then(result => {
+        console.log('phonebook:');
+        result.forEach(person => {
+            console.log(`${person.name} ${person.number}`);
+        });
+        mongoose.connection.close();
+    });
+}
+else if (process.argv.length > 3){
+    const name = process.argv[3];
+    const number = process.argv[4];
 
-const person = new Person({
-    name: name,
-    number: number,
-});
-person.save();
-console.log(`added ${name} number ${number} to phonebook`);
-
-mongoose.connection.close();
+    const person = new Person({
+        name: name,
+        number: number,
+    });
+    person.save()
+    .then(result => {
+        console.log(`added ${name} number ${number} to phonebook`);
+        mongoose.connection.close();
+    });
+}
